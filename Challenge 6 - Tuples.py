a6a971240d391ff468e58021a841b65aaec08a76
“Codeboy3000” <iantfleming1992@gmail.com>
2019-09-06 23:59:52 +0100

    Python code update

--- Challenge 6 - Tuples.py
+++ Challenge 6 - Tuples.py
@@ -1,24 +1,8 @@
-# imelda = "More Mayhem", "Imelda May", 2011, (
-#         (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town"))
-#
-# album, artist, year, tracks = imelda
-#
-# print(album)
-# print(artist)
-# print(year)
-#
-# for song in tracks:
-#     track, title = song
-#     print("\tTrack number {}, Title: {}".format(track, title))
-
 imelda = "More Mayhem", "Imelda May", 2011, (
-    [(1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town")])
+        (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town"))
 
-print(imelda)
-
-imelda[3].append((5, "All For You"))
 album, artist, year, tracks = imelda
-tracks.append((6, "Eternity"))
+
 print(album)
 print(artist)
 print(year)
@@ -26,3 +10,5 @@ print(year)
 for song in tracks:
     track, title = song
     print("\tTrack number {}, Title: {}".format(track, title))
+
+
